2fbc3194e0d839d2bedeba6ce4ac8d189b27d3ac
Paul Aglipay <paglipay@gmail.com>
2020-07-09 10:04:54 -0700

    worker driver frontend

--- public/assets/js/trip.js
+++ public/assets/js/trip.js
@@ -1,7 +1,7 @@
 var nameInput = $("#tripName"),
-		destinationInput = $("#autocomplete"),
-		methodOfTransportInput = $("#methodOfTransport"),
-		arrivalDateInput = $("#datepicker");
+	destinationInput = $("#autocomplete"),
+	methodOfTransportInput = '$("#methodOfTransport")',
+	arrivalDateInput = $("#datepicker");
 
 var url = window.location.search;
 var tripId;
@@ -10,52 +10,60 @@ var userId;
 var updating = false;
 
 function submitTrip(trip) {
-  $.post("/trips/new", trip, function() {
-    window.location.href = "/trips";
-  });
+	console.log('submitTrip')
+	$.post("/trips/new", trip, function () {
+		window.location.href = "/trips";
+	});
 }
 
 function updateTrip(trip) {
-    $.ajax({
-      method: "PUT",
-      url: "/trips",
-      data: trip
-    })
-    .done(function() {
-      window.location.href = "/trips";
-    });
-  }
+	$.ajax({
+		method: "PUT",
+		url: "/trips",
+		data: trip
+	})
+		.done(function () {
+			window.location.href = "/trips";
+		});
+}
 
 function handleFormSubmit(event) {
 	event.preventDefault();
 	// Wont submit the post if we are missing a body, title, or author
-	if (!destinationInput.val().trim() || !methodOfTransportInput.val().trim() || !arrivalDateInput.val()) {
-	  return;
-	}
-	// Constructing a newPost object to hand to the database
-	var newTrip = {
-	  name: nameInput
-	    .val()
-	    .trim(),
-	  destination: destinationInput
-	    .val()
-	    .trim(),
-	  methodOfTransport: methodOfTransportInput
-	  	.val()
-	  	.trim(),
-	  arrivalDate: arrivalDateInput
-	  	.val()
-	  	.trim()
+	// if (!destinationInput.val().trim() || !methodOfTransportInput.val().trim() || !arrivalDateInput.val()) {
+	// 	return;
+	// }
+	// // Constructing a newPost object to hand to the database
+	// var newTrip = {
+	// 	name: nameInput
+	// 		.val()
+	// 		.trim(),
+	// 	destination: destinationInput
+	// 		.val()
+	// 		.trim(),
+	// 	methodOfTransport: methodOfTransportInput
+	// 		.val()
+	// 		.trim(),
+	// 	arrivalDate: arrivalDateInput
+	// 		.val()
+	// 		.trim()
+	// };
+
+		var newTrip = {
+		name: 'nameInput',
+		destination: 'destinationInput',
+		methodOfTransport: 'directions_car',
+		arrivalDate: '2020-07-07 07:00:00'
 	};
 
 	// If we're updating a post run updatePost to update a post
 	// Otherwise run submitPost to create a whole new post
 	if (updating) {
-	  newTrip.id = tripId;
-	  updateTrip(newTrip);
+		newTrip.id = tripId;
+		updateTrip(newTrip);
 	}
 	else {
-	  submitTrip(newTrip);
+		submitTrip(newTrip);
 	}
 }
 
